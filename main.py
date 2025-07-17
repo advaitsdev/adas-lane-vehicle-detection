3edac7b6548c1bc539c65437f6c3db462ddfe6bf
advaitsdev <113018905+advaitsdev@users.noreply.github.com>
2025-07-17 13:21:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import cv2
 import numpy as np
+import torch
+import time
+import sys
+import os
+from ultralytics import YOLO
 
-def process_frame(frame):
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+model = YOLO("yolo11n.pt")
+
+
+def process_lane(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
     edges = cv2.Canny(blur, 50, 150)
@@ -28,23 +39,31 @@ def process_frame(frame):
             x1, y1, x2, y2 = line[0]
             cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
-    combined = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    return combined
+    return cv2.addWeighted(frame, 0.8, line_image, 1, 1)
 
 
-cap = cv2.VideoCapture("project_video.mp4")
-
+cap = cv2.VideoCapture('project_video.mp4')
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    processed = process_frame(frame)
-    cv2.imshow("Lane Detection", processed)
+    start_time = time.time()
 
+    lane_frame = process_lane(frame)
+    results = model.predict(lane_frame, device=device, classes=[2, 3, 5, 7])
+    plot_frame = results[0].plot()
+
+    end_time = time.time()
+    fps = 1 / (end_time - start_time)
+    cv2.putText(plot_frame, f'FPS: {fps:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+
+    cv2.imshow('ADAS: Lane + Vehicle Detection', plot_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+
